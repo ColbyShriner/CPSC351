d24c5e333ab0073b390468c17c22652b3a6e779d
lukee2021 <122477899+lukee2021@users.noreply.github.com>
2024-03-18 19:58:17 -0400

    Update connection.php

--- login/login/connection.php
+++ login/login/connection.php
@@ -1,39 +1,13 @@
 <?php
 
-// $dbhost = "localhost";
-// $dbuser = "root";
-// $dbpass = "";
-// $dbname = "authentication";
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "database351";
 
-// if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
-// {
-// 	die("failed to connect!");
-// }
+if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
+{
+	die("failed to connect!");
+}
 
-$link1 = mysqli_connect("localhost", "root", "", "database351");
- 
-// Check for connection
-if($link1 == true) {
-    echo "database1 Connected Successfully";
-}
-else {
-    die("ERROR: Could not connect " . mysqli_connect_error());
-}
- 
-echo "<br>";
- 
-// Connection of first database
-// Database name => database1
-$con = mysqli_connect("localhost", "root", "", "authentication");
- 
-// Check for connection
-if($con == true) {
-    echo "database2 Connected Successfully";
-}
-else {
-    die("ERROR: Could not connect " . mysqli_connect_error());
-}
- 
-
- 
 ?>
